b3afd4973f08339cf08e7d2ef82fbee901fe457c
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2023-05-02 17:40:27 +0500

    project add

---
+++ app.js
@@ -0,0 +1,23 @@
+const preloader =document.querySelector(".preloader")
+const video =document.querySelector(".videoContainer")
+const switchBtn = document.querySelector(".switch-btn")
+const switchColor = document.querySelector(".switch")
+
+
+window.addEventListener("load", ()=>{
+    preloader.classList.add("hidePreloader")
+})
+
+console.log( switchBtn.classList)
+console.log( switchColor.classList)
+switchBtn.addEventListener("click", ()=>{
+    if(!switchColor.classList.contains("change")){
+        switchColor.classList.add("change")
+        video.pause();
+    }
+    else{
+        switchColor.classList.remove("change")
+        video.play();
+    }
+})
+
